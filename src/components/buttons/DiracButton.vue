106ed1b70f9f6a5125009aeed2922b81e035211e
jsoosiah <5963819+jsoosiah@users.noreply.github.com>
2025-12-19 11:10:02 -0500

    dirac status color on home page

--- src/components/buttons/DiracButton.vue
+++ src/components/buttons/DiracButton.vue
@@ -1,23 +1,39 @@
 <template>
-  <three-state-button
-    v-if="!diracNoFilter"
-    :button-text="`Dirac Live ${ mso?.cal?.diracactive=='on' ? filterTypeToCssClass(mso?.cal?.slots[mso?.cal?.currentdiracslot].filterType, mso?.cal?.slots[mso?.cal?.currentdiracslot].name).toUpperCase() : ''} ${mso?.cal?.diracactive}`"
-    :states="{'off': 0, 'on': 1, 'bypass': 2}"
-    :state-value="mso?.cal?.diracactive"
-    :home-button="props.homeButton"
-    :show-state-indicators="props.homeButton"
-    aria-label="Dirac Live status"
-    min-width="7.125rem"
-    @btn-click="toggleDirac()"
-  />
-  <three-state-button 
-    v-if="diracNoFilter"
-    button-text="Dirac Live No Filter"
-    :states="{'off': 0}"
-    :state-value="'off'"
-    :home-button="props.homeButton"
-    aria-label="Dirac Live status"
-  />
+  <span style="position:relative">
+    <three-state-button
+      v-if="!diracNoFilter"
+      :button-text="`Dirac Live ${ mso?.cal?.diracactive=='on' ? filterTypeToCssClass(mso?.cal?.slots[mso?.cal?.currentdiracslot].filterType, mso?.cal?.slots[mso?.cal?.currentdiracslot].name).toUpperCase() : ''} ${mso?.cal?.diracactive}`"
+      :states="{'off': 0, 'on': 1, 'bypass': 2}"
+      :state-value="mso?.cal?.diracactive"
+      :home-button="props.homeButton"
+      :show-state-indicators="props.homeButton"
+      aria-label="Dirac Live status"
+      min-width="9rem"
+      @btn-click="toggleDirac()"
+    />
+    <three-state-button 
+      v-if="diracNoFilter"
+      button-text="Dirac Live No Filter"
+      :states="{'off': 0}"
+      :state-value="'off'"
+      :home-button="props.homeButton"
+      aria-label="Dirac Live status"
+    />
+    <div class="ml-2" style="position:absolute;top:1.8rem;left:0.75rem;z-index:1">
+      <template v-if="diracErrorState === 'INACTIVE'">
+        <span style="width:7.5rem;height:0.25rem;padding:0;opacity:0.5" class="badge badge-secondary">&nbsp;</span>
+      </template>
+      <template v-else-if="diracErrorState === 'GREEN'">
+        <span style="width:7.5rem;height:0.25rem;padding:0" class="badge badge-success">&nbsp;</span>
+      </template>
+      <template v-else-if="diracErrorState === 'YELLOW'">
+        <span style="width:7.5rem;height:0.25rem;padding:0" class="badge badge-warning">&nbsp;</span>
+      </template>
+      <template v-else>
+        <span style="width:7.5rem;height:0.25rem;padding:0" class="badge badge-danger">&nbsp;</span>
+      </template>
+    </div>
+  </span>
 </template>
 
 <script>
@@ -36,9 +52,9 @@
     },
     setup(props) {
 
-      const { mso, toggleDirac, diracNoFilter, filterTypeToCssClass } = useMso();
+      const { mso, toggleDirac, diracNoFilter, filterTypeToCssClass, diracErrorState } = useMso();
 
-      return { mso, toggleDirac, props, diracNoFilter, filterTypeToCssClass };
+      return { mso, toggleDirac, props, diracNoFilter, filterTypeToCssClass, diracErrorState };
     }
   }
 </script>
@@ -55,4 +71,5 @@
   .yellow-text-btn {
     color:yellow;
   }
+
 </style>
--- src/components/buttons/DiracButtonGroup.vue
+++ src/components/buttons/DiracButtonGroup.vue
@@ -15,27 +15,28 @@
     :state-value="0"
     :home-button="props.homeButton"
   />
+
   <span class="ml-2">
     <template v-if="diracErrorState === 'INACTIVE'">
       <span v-tooltip="{'message': 'Dirac Live filter is not active.'}" :id="`${props.tooltipId}-tooltipinactive`">
-        <span class="badge badge-secondary">Inactive</span>
+        <span style="width:4rem" class="badge badge-secondary">Inactive</span>
         <font-awesome-icon class="ml-1" :icon="['fas', 'question-circle']" />
       </span>
     </template>
     <template v-else-if="diracErrorState === 'GREEN'">
       <span v-tooltip="{'message': 'Dirac Live calibration is active and filters are operating as expected.'}" :id="`${props.tooltipId}-tooltipsuccess`">
-        <span class="badge badge-success">Active</span>
+        <span style="width:4rem" class="badge badge-success">Active</span>
       </span>
     </template>
     <template v-else-if="diracErrorState === 'YELLOW'">
       <span v-tooltip="{'message': 'Dirac Live calibration is in an unexpected state. Try toggling Dirac Live off and back on to fix it.'}" :id="`${props.tooltipId}-tooltipwarning`">
-        <span class="badge badge-warning">Warning</span>
+        <span style="width:4rem" class="badge badge-warning">Warning</span>
         <font-awesome-icon class="ml-1" :icon="['fas', 'question-circle']" />
       </span>
     </template>
     <template v-else>
       <span v-tooltip="{'message': 'Dirac Live filter has failed to load. Try rebooting the HTP-1 to fix it.'}" :id="`${props.tooltipId}-tooltiperror`">
-        <span class="badge badge-danger">Error</span>
+        <span style="width:4rem" class="badge badge-danger">Error</span>
         <font-awesome-icon class="ml-1" :icon="['fas', 'question-circle']" />
       </span>
     </template>
